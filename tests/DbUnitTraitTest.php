adcb5da6263aa36e4630446e9ba79c7db1ec6ca8
ngyuki <ngyuki.ts@gmail.com>
2015-01-18 18:46:41 +0900

    For php 5.3

--- tests/DbUnitTraitTest.php
+++ tests/DbUnitTraitTest.php
@@ -1,33 +1,6 @@
 <?php
 namespace Tests;
 
-use ngyuki\DbUnitHelper\DbUnitTrait;
-
-class DbUnitTraitTest extends AbstractTestCase
-{
-    use DbUnitTrait;
-
-    /**
-     * @test
-     * @dataSet fooDataSet
-     */
-    public function foo()
-    {
-        $stmt = self::$pdo->prepare("select * from t order by id");
-        $stmt->execute();
-
-        assertThat($stmt->fetchAll(), equalTo(array(
-            array('id' => 1),
-            array('id' => 2),
-        )));
-    }
-
-    public function fooDataSet()
-    {
-        return $this->dbunit->createYamlDataSetByInline("
-            t:
-              - id: 1
-              - id: 2
-        ");
-    }
+if (version_compare(PHP_VERSION, '5.4') >= 0) {
+    require __DIR__ . '/DbUnitTraitTestFor54.php';
 }
